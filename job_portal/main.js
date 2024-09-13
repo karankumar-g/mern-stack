cef9de28405132113ad01b5646fe99071d82848d
karankumar-g <karankumar.g0csa@gmail.com>
2024-09-13 16:04:35 +0530

    JWT Token Generating and Checking Session

--- job_portal/main.js
+++ job_portal/main.js
@@ -6,12 +6,25 @@ const { MongoClient, ObjectId } = require("mongodb");
 var cors = require("cors");
 app.use(cors());
 
+app.use("/api/", (req, res, next) => {
+  let { token } = req.headers;
+  if (token == "" || token == undefined) {
+    res.status(401).json({ Message: "Please add The Token" });
+  } else {
+    if (jwt.verify(token, "SECRET")) {
+      next();
+    } else {
+      res.json({ msg: "Providre the Correct Token" });
+    }
+  }
+}); // MIDDLEWARE
+
 const url = "mongodb+srv://karankumar:karan2909@cluster0.viptk.mongodb.net/";
 const client = new MongoClient(url);
 
 const dbName = "jobportal";
 
-app.post("/create_jobs", async (req, res) => {
+app.post("/api/create_jobs", async (req, res) => {
   let body = req.body;
   let data = {
     name: body["name"],
@@ -25,7 +38,7 @@ app.post("/create_jobs", async (req, res) => {
   res.json({ msg: "Successfully created a job" });
 });
 
-app.get("/list_jobs", async (req, res) => {
+app.get("/api/list_jobs", async (req, res) => {
   await client.connect();
   let db = client.db("office");
   let data = await db.collection("jobportal").find({}).toArray();
@@ -43,7 +56,7 @@ app.get("/list_jobs", async (req, res) => {
 //   res.json({ msg: "Updated the requirements" });
 // });
 
-app.put("/update_job", async (req, res) => {
+app.put("/api/update_job", async (req, res) => {
   let { id, name, company, requirements } = req.body;
   await client.connect();
   let db = client.db("office");
@@ -57,7 +70,7 @@ app.put("/update_job", async (req, res) => {
   res.json({ msg: "Updated the requirements" });
 });
 
-app.delete("/delete_job", async (req, res) => {
+app.delete("/api/delete_job", async (req, res) => {
   let { id } = req.query;
   await client.connect();
   let db = client.db("office");
@@ -66,4 +79,34 @@ app.delete("/delete_job", async (req, res) => {
   res.json({ msg: "Deleted the Job" });
 });
 
+var jwt = require("jsonwebtoken");
+
+app.post("/login", async (req, res) => {
+  await client.connect();
+  let { name, password } = req.body;
+  let db = client.db("office");
+  const teachers = await db
+    .collection("teachers")
+    .find({ name: name, password: password })
+    .toArray();
+  if (teachers.length > 0) {
+    var token = jwt.sign({ name: teachers[0]["name"] }, "SECRET");
+    res.json({ message: "Login Successful", token: token });
+  } else {
+    res.status(401).json({ error: "Password incorrect" });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  let body = req.body;
+  let data = {
+    name: body["name"],
+    email: body["email"],
+    password: body["password"],
+  };
+  await client.connect();
+  let db = client.db("office");
+  await db.collection("teachers").insertOne(data);
+  res.status(200).json({ message: "Created New Teacher Record!!" });
+});
 app.listen(8080);
